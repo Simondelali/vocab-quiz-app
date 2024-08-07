b0e88ad4d8d889503ba7e5d93a89ba2d57559c88
Simondelali <atiegardelali@gmail.com>
2024-08-07 12:24:05 +0000

    Add vocabs

--- src/app/data/vocabularies.js
+++ src/app/data/vocabularies.js
@@ -5,5 +5,9 @@ export const vocabularies = [
     { id: 3, word: 'Cacophony', definition: 'A harsh, discordant mixture of sounds' },
     { id: 4, word: 'Diligent', definition: 'Having or showing persistent and hardworking effort' },
     { id: 5, word: 'Eloquent', definition: 'Fluent or persuasive in speaking or writing' },
-    // Add more words here...
+    { id: 6, word: 'Abstruse', definition: 'Difficult to understand' },
+    { id: 7, word: 'Apt', definition: 'Appropriate, inclined' },
+    { id: 8, word: 'Cajole', definition: 'Persuade to do something by sustainined coaxing or flattery' },
+    { id: 9, word: 'Compendious', definition: 'Containing the essential facts of something in a comprehebsive but consive way' },
+    { id: 10, word: 'Concur', definition: 'Agree' },
   ];
